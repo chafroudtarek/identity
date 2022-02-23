1ceac1938cc69de2c11b5ecc844df807c58b4a15
Tarek chafroud <chafroudtarek6@gmail.com>
2022-02-23 09:17:42 +0100

    remove logs

--- utils/kafka.js
+++ utils/kafka.js
@@ -5,27 +5,27 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["localhost:9092", "localhost:9092"],
+  brokers: ["localhost:9092","localhost:9093"],
 });
 
 const producer = kafka.producer();
 
 export const run = async (res) => {
 
-  const topicMessages = [
+  
+  // Producing
+  
+  await producer.connect();
+  await producer.sendBatch([ 
     {
       topic: 'my-topic',
       messages:[{ value: JSON.stringify(res)}],
     },
     {
-      topic: 'test1',
+      topic: 'topic',
       messages:[{ value: JSON.stringify(res)}],
-    },
-  ]
-  // Producing
-
-  await producer.connect();
-  await producer.sendBatch({ topicMessages })
+    }
+  ])
   // await producer.send({
   //   topic: 'my-topic',
   //   messages:[{ value: JSON.stringify(res)}] ,
